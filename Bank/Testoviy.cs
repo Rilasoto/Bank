0a314ffbb988e3361830eddb8c3694a2d7527e6b
dzyabenkoea <gorich60@gmail.com>
2017-09-18 22:48:03 +0500

    тестовый класс

---
+++ Bank/Testoviy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Testoviy
+    {
+        public void GG()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
