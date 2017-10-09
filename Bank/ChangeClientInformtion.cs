03b6324c7835c62be7d33f572c1a2e4f96c2b237
Михаил <@OVERLORD-2>
2017-10-09 15:50:53 +0500

    Сделал форму редактирвоания пользователя(пока что без кода)

---
+++ Bank/ChangeClientInformtion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class ChangeClientInformtion : Form
+    {
+        public ChangeClientInformtion()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeClientInformtion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
